352de2700941e830939c5704bd6b3eade33cdfe4
Yogesh Rajmane <yogesh.rajmane91@gmail.com>
2020-03-23 07:49:01 +0530

    End Section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,14 +1,28 @@
-import React,{Component} from 'react';
+import React,{Component,Fragment} from 'react';
+import {Button} from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
+import { Container} from 'semantic-ui-react';
 
 class App extends Component
 {
   render()
   {
     return (
-      <div >
-          <p>Yogesh Rajmane</p>
-          <p>Nagesh Rajmane</p>
-      </div>
+      <Fragment>
+        <NavBar></NavBar>
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+        {/* <h1>Rev-vents</h1>
+        <EventDashboard></EventDashboard>
+   
+        {/* <button className="ui icon button">
+          <i className="smile icon"></i>
+          CSS Button
+        </button>
+        <Button icon="smile" content="React Button"></Button> */}
+      </Fragment> 
     );    
   }
 
